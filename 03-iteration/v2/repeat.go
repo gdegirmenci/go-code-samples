a81ddfc263c8550ccbb2a67387880f0317e7fa60
Gökhan Değirmenci <gdegirmenci0@icloud.com>
2020-02-23 22:32:22 +0300

    Adding benchmark repeat to iteration

--- 03-iteration/v2/repeat.go
+++ 03-iteration/v2/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 3
+
 func Repeat(character string) string {
 	var repeated string
 
-	for i := 0; i < 4; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated = repeated + character
 	}
 
